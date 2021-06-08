7d911c72f28a5b76bd75642db440e52bd95c7ae2
wesleyotto <wesley.otto@hotmail.com>
2021-06-08 17:26:32 -0300

    JS Material

---
+++ 6 - JS/02 - PrecedenciaDeExecucao/script.js
@@ -0,0 +1,6 @@
+// Cometário simples
+
+/*Bloco de código*/
+alert('Olá, Seja bem vindo !')
+document.getElementById('nome').value = 'Oi'
+
---
+++ 6 - JS/03 - Variaveis/script.js
@@ -0,0 +1,23 @@
+
+//String
+
+var texto = 'Curso de JavaScript'
+
+//Number
+
+var numeroInteiro = -7 
+var numeroFracionario = 10.75
+
+// Bollean
+
+var verdadeiro = true
+var falso = false
+
+// Comandos legais para usar para debug
+
+//alert(numeroFracionado) //abrir um dialog
+
+//document.write(texto)  //escreve no doc
+
+//console.log(numeroInteiro) //mostra o resultado no console
+
